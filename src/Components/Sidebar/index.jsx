414c2247d5d910d5487f3606142aeb6d7f3d21e6
Arunava Kundu <arunava.kundu@iemlabs.com>
2023-09-22 17:21:12 +0530

    fixing layout

--- src/Components/Sidebar/index.jsx
+++ src/Components/Sidebar/index.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { HiMenuAlt3 } from 'react-icons/hi';
 import { RiDashboardFill } from 'react-icons/ri';
+import { BiTransferAlt, BiLogOut } from 'react-icons/bi';
 import { Link } from 'react-router-dom';
 
 function index() {
@@ -39,7 +40,7 @@ function index() {
   return (
     <div
       className={`bg-gradient-to-b from-[#035685] to-[#053E5E] h-full ${
-        device === 'mobile' ? `${active ? 'w-72' : ''}` : 'w-16'
+        device === 'mobile' ? `${active ? 'w-72' : 'w-16'}` : 'w-16'
       } duration-500 px-4 py-5 flex flex-col justify-between `}>
       <div>
         <div className="py-3 flex justify-end">
@@ -51,15 +52,15 @@ function index() {
             onClick={() => setActive(!active)}
           />
         </div>
-        <div className=" top-20 flex flex-col gap-4 relative">
+        <div className=" flex flex-col top-[20%] right-1 gap-20 relative">
           <Link
             to={'/'}
-            className={`group flex items-center text-sm gap-3.5 font-medium p-2 hover:bg-blue-100 bg-opacity-10 rounded-md`}>
+            className={`group flex text-sm gap-3.5 font-medium p-2 hover:bg-white hover:bg-opacity-50 rounded-md`}>
             <div>
               {React.createElement(RiDashboardFill, {
                 size: 26,
                 className: `${selected === 'Dashboard' ? 'text-[#fff]' : 'text-[#666]'} ${
-                  active ? 'mr-4' : ''
+                  active ? 'm-auto' : 'm-auto'
                 }`,
                 onClick: () => setSelected('Dashboard')
               })}
@@ -83,6 +84,72 @@ function index() {
                 active ? ' hidden' : ''
               } absolute left-48 bg-white font-semibold whitespace-pre rounded-md drop-shadow-lg px-0 py-0 w-0 overflow-hidden group-hover:px-2 group-hover:py-1 group-hover:left-14 group-hover:duration-300 group-hover:w-fit`}>
               Dashboard
+            </h2>
+          </Link>
+          <Link
+            to={'/device-list'}
+            className={`group flex text-sm gap-3.5 font-medium p-2 hover:bg-white hover:bg-opacity-50 rounded-md`}>
+            <div>
+              {React.createElement(BiTransferAlt, {
+                size: 26,
+                className: `${selected === 'DeviceList' ? 'text-[#fff]' : 'text-[#666]'} ${
+                  active ? 'm-auto' : 'm-auto'
+                }`,
+                onClick: () => setSelected('DeviceList')
+              })}
+            </div>
+            <h2
+              style={{ transitionDelay: `${0 * 2}00ms` }}
+              className={`${
+                selected === 'DeviceList'
+                  ? `text-[#fff] whitespace-pre duration-500 ${
+                      !active && 'opacity-0 translate-x-28 overflow-hidden'
+                    }`
+                  : `text-[#666666] whitespace-pre duration-500 ${
+                      !active && 'opacity-0 translate-x-28 overflow-hidden'
+                    }`
+              }`}
+              onClick={() => setSelected('DeviceList')}>
+              Device List
+            </h2>
+            <h2
+              className={` ${
+                active ? ' hidden' : ''
+              } absolute left-48 bg-white font-semibold whitespace-pre rounded-md drop-shadow-lg px-0 py-0 w-0 overflow-hidden group-hover:px-2 group-hover:py-1 group-hover:left-14 group-hover:duration-300 group-hover:w-fit`}>
+              Device List
+            </h2>
+          </Link>
+          <Link
+            to={'/sign-in'}
+            className={`group flex text-sm gap-3.5 font-medium p-2 hover:bg-white hover:bg-opacity-50 rounded-md`}>
+            <div>
+              {React.createElement(BiLogOut, {
+                size: 26,
+                className: `${selected === 'Logout' ? 'text-[#fff]' : 'text-[#666]'} ${
+                  active ? 'm-auto' : 'm-auto'
+                }`,
+                onClick: () => setSelected('Logout')
+              })}
+            </div>
+            <h2
+              style={{ transitionDelay: `${0 * 2}00ms` }}
+              className={`${
+                selected === 'Logout'
+                  ? `text-[#fff] whitespace-pre duration-500 ${
+                      !active && 'opacity-0 translate-x-28 overflow-hidden'
+                    }`
+                  : `text-[#666666] whitespace-pre duration-500 ${
+                      !active && 'opacity-0 translate-x-28 overflow-hidden'
+                    }`
+              }`}
+              onClick={() => setSelected('Logout')}>
+              Logout
+            </h2>
+            <h2
+              className={` ${
+                active ? ' hidden' : ''
+              } absolute left-48 bg-white font-semibold whitespace-pre rounded-md drop-shadow-lg px-0 py-0 w-0 overflow-hidden group-hover:px-2 group-hover:py-1 group-hover:left-14 group-hover:duration-300 group-hover:w-fit`}>
+              Logout
             </h2>
           </Link>
         </div>
---
+++ src/Pages/DeviceList/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function index() {
+  return <div>DeviceList</div>;
+}
+
+export default index;
