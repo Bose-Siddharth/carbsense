a5afc3dc5827bae3ad04487f92b5527493a5b655
Arunava Kundu <arunava.kundu@iemlabs.com>
2023-09-25 12:52:57 +0530

    monitor page layout

--- src/Pages/Monitor/index.jsx
+++ src/Pages/Monitor/index.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
 
 function index() {
-  return <div>Monitor</div>;
+  return (
+    <div className="flex flex-col gap-10">
+      <div>
+        <h1>Monitor</h1>
+      </div>
+      <div className="flex gap-5">
+        <div className="bg-white w-[50%] h-full">1</div>
+        <div className="bg-white w-[25%] h-full">2</div>
+        <div className="bg-white w-[25%] h-full">3</div>
+      </div>
+      <div className="flex gap-5">
+        <div className="bg-white w-[50%] h-full">4</div>
+        <div className="bg-white w-[25%] h-full">5</div>
+        <div className="bg-white w-[25%] h-full">6</div>
+      </div>
+    </div>
+  );
 }
 
 export default index;
