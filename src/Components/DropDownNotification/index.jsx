e41ddee5d75499b363c15835024cc19e2f0545c1
Arunava Kundu <arunava.kundu@iemlabs.com>
2023-09-25 18:38:28 +0530

    msd dropdown

--- src/Components/DropDownNotification/index.jsx
+++ src/Components/DropDownNotification/index.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import './DropDownNotification.css';
-import { BiSolidMessageSquareDots } from 'react-icons/bi';
 
 function index() {
   return (
     <>
-      <BiSolidMessageSquareDots />
       <div className="flex flex-col dropDownProfile">
         <ul className="flex flex-col gap-4">
-          <li>1</li>
-          <li>2</li>
-          <li>3</li>
+          <li className="bg-blue-400 h-20 rounded-lg p-3">1</li>
+          <li className="bg-blue-400 h-20 rounded-lg p-3">2</li>
+          <li className="bg-blue-400 h-20 rounded-lg p-3">3</li>
         </ul>
       </div>
     </>
--- src/Pages/Monitor/index.jsx
+++ src/Pages/Monitor/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { RiMessage3Fill, RiArrowGoBackFill } from 'react-icons/ri';
 import DropDownNotification from '../../Components/DropDownNotification';
 import Measurements from '../../Components/Monitor/Measurements';
 import OilLevel from '../../Components/Monitor/OilLevel';
@@ -8,11 +9,14 @@ function index() {
     <div className="flex flex-col  gap-10">
       <div className="flex justify-between border border-black border-s-4 border-r-0 border-t-0 border-b-0">
         <h1 className="font-semibold text-4xl">Monitor</h1>
-        <div className="flex ">
-          <div>
+        <div className="flex w-1/12">
+          <div className="flex flex-1 justify-center items-center">
+            <RiMessage3Fill className="text-3xl" />
             <DropDownNotification />
           </div>
-          <div>2</div>
+          <div className="flex flex-1 justify-center items-center">
+            <RiArrowGoBackFill className="text-3xl" />
+          </div>
         </div>
       </div>
       <div className="flex gap-5">
